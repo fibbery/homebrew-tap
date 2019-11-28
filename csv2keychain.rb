778ffe139d11556ed2de4217be249dc67f5ea9c1
Nenghua Jiang <jiangnenghua1992@gmail.com>
2019-11-28 20:08:39 +0800

    Update csv2keychain.rb

--- csv2keychain.rb
+++ csv2keychain.rb
@@ -4,7 +4,7 @@
 class Csv2keychain < Formula
   desc "command-line for load password csv file from chrome to macos keychain"
   homepage ""
-  url "https://github.com/fibbery/csv2keychain/releases/download/1.0.1/csv2keychain_1.0.1_Darwin_i386.tar.gz"
+  url "https://github.com/fibbery/csv2keychain/releases/download/1.0.2/csv2keychain_1.0.2_Darwin_i386.tar.gz"
   sha256 "2a9f1c8e4a7bade4851c918d486600c029c3136071c1537668f2ebb4a5342275"
   # depends_on "cmake" => :build
 
